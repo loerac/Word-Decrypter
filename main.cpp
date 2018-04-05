5827a5da061ac0bf577d07e6281be759cb7c4aa1
Christian Loera <chatoloera18@gmail.com>
2018-04-05 04:06:21 -0500

    A lot was updated
    I started on this project 2 years ago on another laptop that I don't have anymore. I am working on updating this current code to what it was 2 years ago.

--- main.cpp
+++ main.cpp
@@ -45,89 +45,88 @@ int main(int argc, char* argv[]) {
         }
     }
 
-	vector<string> wordList;
+    vector<string> wordList;
     string line, resp, en_letter;
 
-	ofstream fout;
-	ifstream fin;
+    ofstream fout;
+    ifstream fin;
 
-	int wordCount = 0, run = 1;
+    int wordCount = 0, run = 1;
 	
     fin.open("wordList.txt");
-	if (!fin) { cout << "Cannot find wordList.txt" << endl; return 2; }
-	while (!(run & 8)) {
+    if (!fin) { cout << "Cannot find wordList.txt" << endl; return 2; }
+    while (!(run & 8)) {
         run = 0;
-		cout << "\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\t\t    Please wait..." << endl;
+	cout << "\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\t\t    Please wait..." << endl;
     	while (getline(fin, line)) { wordCount++; }
-	    fin.close();
+	fin.close();
 
-		fin.open("wordList.txt");
-		string filename = en + ".txt";
-		fout.open(filename.c_str());
-		while (getline(fin, line)) {
-			if (((line.size()-1)==en.size()) && amountLetters(line, en) && forgetWord(line, out) && checkWord(line, in)) {
-				wordList.push_back(line);
-				cout << "Found: " << line << '\r';
-			}
-		}
-		fin.close();
+	fin.open("wordList.txt");
+	string filename = en + ".txt";
+	fout.open(filename.c_str());
+	while (getline(fin, line)) {
+	    if (((line.size()-1)==en.size()) && amountLetters(line, en) && forgetWord(line, out) && checkWord(line, in)) {
+		wordList.push_back(line);
+		cout << "Found: " << line << '\r';
+	    }
+	}
+	fin.close();
         cout << "\n============================================================\n" << endl;
-		cout << "Current word: " << en << endl;
-		cout << "Amount of words: " << wordList.size() << endl;
-	    cout << "\n============================================================\n" << endl;
-		cout << "I can shorten the list down if you know where the letter(s) go." << endl;
-		cout << "I can also find look for sequence of letters that you know." << endl;
-		cout << "Do you know where any of the letter(s) go in the word or a sequence [y/n] >> ";
-		cin >> resp;
+	cout << "Current word: " << en << endl;
+	cout << "Amount of words: " << wordList.size() << endl;
+	cout << "\n============================================================\n" << endl;
+	cout << "I can shorten the amount of words if you know where the letter(s) go." << endl;
+	cout << "Do you know where any of the letter(s) go in the word or a sequence [y/n] >> ";
+	cin >> resp;
         resp = tolower(resp[0]);
-	    if (resp == "y") {
-    		cout << "\n============================================================\n" << endl;
-	    	while (resp == "y") {
-				cout << "Enter the letter from the current word and then the letter it goes with >> ";
-				cin >> en_letter >> resp;
-				en = letterPlacement(en, wordList, en_letter[0], resp[0]);
+	if (resp == "y") {
+    	    cout << "\n============================================================\n" << endl;
+	    while (resp == "y") {
+		cout << "Enter the letter from the current word and then the letter it goes with >> ";
+		cin >> en_letter >> resp;
+		en = letterPlacement(en, wordList, en_letter[0], resp[0]);
 
                 cout << "\n============================================================\n" << endl;
-				cout << "Current word: " << en << endl;
-				cout << "Amount of words: " << wordList.size() << endl;
-				cout << "\n============================================================\n" << endl;
-
-				if (wordList.size() == 0) {
-					cout << "Cannot find word from input you gave me...." << endl;
-					resp = "n";
-				} else {
-					if (wordList.size() <= 10) {
-						cout << "Current word list..." << endl;
-						for (int i = 0; i < wordList.size(); i++)
-							cout << i + 1 << ": " << wordList[i] << endl;
-							cout << "\n============================================================\n" << endl;
-					}
-
-				    cout << "Do you know any other letter or sequence [y/n] >> ";
-					cin >> resp;
-					if (resp == "y") { cout << "\n============================================================\n" << endl; }
-				}
-		    }
-        }
+		cout << "Current word: " << en << endl;
+		cout << "Amount of words: " << wordList.size() << endl;
 		cout << "\n============================================================\n" << endl;
-		for (int i = 0; i < wordList.size(); i++) {
-			if ((i + 1) == wordList.size()) { fout << wordList[i]; }
-			else { fout << wordList[i] << "\n"; }
+
+		if (wordList.size() == 0) {
+		    cout << "Cannot find word from input you gave me...." << endl;
+		    resp = "n";
+		} else {
+		    if (wordList.size() <= 10) {
+			cout << "Current word list..." << endl;
+			for (int i = 0; i < wordList.size(); i++)
+			    cout << i + 1 << ": " << wordList[i] << endl;
+			cout << "\n============================================================\n" << endl;
+                    }
+
+		    cout << "Do you know any other letter or sequence [y/n] >> ";
+		    cin >> resp;
+		    if (resp == "y") { cout << "\n============================================================\n" << endl; }
+		}
+            }
         }
-		fout.close();
+	cout << "\n============================================================\n" << endl;
+	for (int i = 0; i < wordList.size(); i++) {
+	    if ((i + 1) == wordList.size()) { fout << wordList[i]; }
+	    else { fout << wordList[i] << "\n"; }
+        }
+	fout.close();
 		
         while(!(run & 4) && !(run & 8)) {
             if(!(run & 2)) { cout << "Would you like to open the file or enter a new word or exit [o/w/e] >> "; }
             else { cout << "Would you like to enter a new word or exit [w/e] >> "; }
-		    cin >> resp;
+	    cin >> resp;
             resp = tolower(resp[0]);
-    		if (resp == "o" && !(run & 2)) {
-    			for (int i = 0; i < wordList.size(); i++) { cout << i + 1 << ": " << wordList[i] << endl; }
+    	    if (resp == "o" && !(run & 2)) {
+    		for (int i = 0; i < wordList.size(); i++) { cout << i + 1 << ": " << wordList[i] << endl; }
                 run |= 2;
-    		} else if (resp == "w") {
-    			cout << "\n============================================================\n" << endl;
-    			cout << "\t\t\t\t\t\t\t    Please wait..." << endl;
-    			wordList.erase(wordList.begin(), wordList.end());
+    	    } else if (resp == "w") {
+    		cout << "\n============================================================\n" << endl;
+    		cout << "\t\t\t\t\t\t\t    Please wait..." << endl;
+    		wordList.erase(wordList.begin(), wordList.end());
                 cout << "Enter encrypted word >> ";
                 cin >> en;
                 cout << "Enter known letters in the encrypted word >> ";
@@ -135,12 +134,10 @@ int main(int argc, char* argv[]) {
                 cout << "Enter known letters that aren't in the encrypted word >> ";
                 cin >> out;
                 run |= 4;
-    		} else { run |= 8; }
+    	    } else { run |= 8; }
         }
-	    cout << "Look in the " << filename << " for the decrypted word(s)." << endl;
-    }
-
-    return 0;
+	cout << "Look in the " << filename << " for the decrypted word(s)." << endl;
+    } return 0;
 }
 
 // Finding all the common letters in both strings
@@ -155,125 +152,125 @@ bool commonLetters(string &common, string in, string out) {
 
 // Checks the word to see if it has the letter that I know for sure isn't in the phrase
 bool forgetWord(string line, string letter) {
-	int count = 0, i = 0, j = 0;
-	bool found = true;
+    int count = 0, i = 0, j = 0;
+    bool found = true;
 
-	while (letter.size() != count) {
-		while (found) {
-			if (line[i] == letter[j]) { return false; }
-			if (i == line.size() && count < letter.size()) { found = false; }
-			i++;
-		}
-		count++;
-		i = 0;
-		j++;
-		found = true;
-	} return true;
+    while (letter.size() != count) {
+	while (found) {
+	    if (line[i] == letter[j]) { return false; }
+	    if (i == line.size() && count < letter.size()) { found = false; }
+	     i++;
+	}
+	count++;
+	i = 0;
+	j++;
+	found = true;
+    } return true;
 }
 
 // Check all the words to see if it has the letters in the word
 bool checkWord(string line, string letter) {
-	int count = 0, i = 0, j = 0;
-	bool found = true;
+    int count = 0, i = 0, j = 0;
+    bool found = true;
 
-	while (letter.size() != count) {
-		while (found) {
-			if (line[i] == letter[j]) { found = false; }
-			if (i == line.size() && count < letter.size()) { return false; }
-			i++;
-		}
-		count++;
-		i = 0;
-		j++;
-		found = true;
-	} return true;
+    while (letter.size() != count) {
+	while (found) {
+	    if (line[i] == letter[j]) { found = false; }
+	    if (i == line.size() && count < letter.size()) { return false; }
+	    i++;
+	}
+	count++;
+	i = 0;
+	j++;
+	found = true;
+    } return true;
 }
 
 //Checks to see if the word from the file has the same amount of characters in each
 bool amountLetters(string line, string cypher) {
-	int cypherCount = 0, lineCount = 0;
-	char cypherLetter, lineLetter;
+    int cypherCount = 0, lineCount = 0;
+    char cypherLetter, lineLetter;
 
-	for (int i = 0; i < cypher.size(); i++) {
-		cypherLetter = cypher[i];
-		for (int j = 1; j < cypher.size(); j++) {
-			if (cypher[j] == cypherLetter) { cypherCount++; }
+    for (int i = 0; i < cypher.size(); i++) {
+	cypherLetter = cypher[i];
+	for (int j = 1; j < cypher.size(); j++) {
+	    if (cypher[j] == cypherLetter) { cypherCount++; }
         }
 
-		lineLetter = line[i];
-		for (int j = 1; j < line.size(); j++) {
-			if (line[j] == lineLetter) {lineCount++; }
+	lineLetter = line[i];
+	for (int j = 1; j < line.size(); j++) {
+	    if (line[j] == lineLetter) {lineCount++; }
         }
-	}
-	if (cypherCount == lineCount) {	return true; }
-	return false;
+    }
+    if (cypherCount == lineCount) { return true; }
+    return false;
 }
 
 // Checks to see if the replacing of the letters won't effect the other letters
 string letterPlacement(string currentWord, vector<string>& wordList, char letter, char resp) {
-	string word;
-	char temp = '0';
-	bool valid = true;
-	int i = 0, place = 0, count = 0, lCount = 0;
+    string word;
+    char temp = '0';
+    bool valid = true;
+    int i = 0, place = 0, count = 0, lCount = 0;
 
-	for (int m = 0; m < currentWord.size(); m++) {
-		if (currentWord[m] == resp) {
-			currentWord[m] = temp;
-			place = i;
-			count++;
-		} else if (currentWord[m] == letter) {
-			currentWord[m] = resp;
-			place = i;
-			placement(place, wordList, currentWord);
-		} i++;
-	}
-	for (int j = 0; j < currentWord.size(); j++) {
-		if (currentWord[j] == '0') { currentWord[j] = letter; }
+    for (int m = 0; m < currentWord.size(); m++) {
+        if (currentWord[m] == resp) {
+	    currentWord[m] = temp;
+	    place = i;
+	    count++;
+	} else if (currentWord[m] == letter) {
+	    currentWord[m] = resp;
+	    place = i;
+	    placement(place, wordList, currentWord);
+	} i++;
+    }
+    for (int j = 0; j < currentWord.size(); j++) {
+	if (currentWord[j] == '0') { currentWord[j] = letter; }
     } return currentWord;
 }
 
 // Removing the words that don't have the letter in the correct spot
 void placement(int place, vector<string>& wordList, string currentWord) {
-	string word;
-	for (int i = 0; i < wordList.size(); i++) {
-		if (i < 0) { i = 0; }
-		word = wordList[i];
-		if (word[place] != currentWord[place]) {
-			cout << "Removed: " << word << '\r';
-			wordList.erase(wordList.begin() + i);
-			i--;
-		}
+    string word;
+    for (int i = 0; i < wordList.size(); i++) {
+	if (i < 0) { i = 0; }
+	word = wordList[i];
+	if (word[place] != currentWord[place]) {
+	    cout << "Removed: " << word << '\r';
+	    wordList.erase(wordList.begin() + i);
+	    i--;
 	}
+    }
 }
 
 //Checks to see if the known sequence is in the word
 void sequence(string word, vector<string>& wordList) {
-	string stringTemp, wordTemp;
-	int intTemp = 0, sequenceSize = word.size(), flag = 0;
+    string stringTemp, wordTemp;
+    int intTemp = 0, sequenceSize = word.size(), flag = 0;
 
-	for (int i = 0; i < wordList.size(); i++) {
-		stringTemp = wordList[i];
-		cout << "Vector: " << stringTemp << endl;
-		for (int j = 0; j < stringTemp.size(); j++) {
-			if (intTemp <= stringTemp.size() - sequenceSize) {
-				for (int k = 0; k < sequenceSize; k++) {
-					wordTemp += stringTemp[intTemp];
-					intTemp++;
-				}
-			}
-			cout << "Currently: " << wordTemp << endl;
-			intTemp -= sequenceSize - 1;
-			if (wordTemp == word) {
-				cout << wordTemp << " " << word << endl;
-				flag = 1;
-			}
-			wordTemp = "";
+    for (int i = 0; i < wordList.size(); i++) {
+	stringTemp = wordList[i];
+	cout << "Vector: " << stringTemp << endl;
+	for (int j = 0; j < stringTemp.size(); j++) {
+            if (intTemp <= stringTemp.size() - sequenceSize) {
+		for (int k = 0; k < sequenceSize; k++) {
+		    wordTemp += stringTemp[intTemp];
+		    intTemp++;
 		}
-		if (flag == 0) {
-			cout << "Removed: " << wordList[i] << endl;
-			wordList.erase(wordList.begin() + i);
-		}
-		intTemp = 0;
-		flag = 0;
-	}
+	    }
+	    cout << "Currently: " << wordTemp << endl;
+	    intTemp -= sequenceSize - 1;
+	    if (wordTemp == word) {
+	        cout << wordTemp << " " << word << endl;
+	        flag = 1;
+	    }
+	    wordTemp = "";
+        }
+        if (flag == 0) {
+            cout << "Removed: " << wordList[i] << endl;
+            wordList.erase(wordList.begin() + i);
+        }
+        intTemp = 0;
+        flag = 0;
+    }
 }
